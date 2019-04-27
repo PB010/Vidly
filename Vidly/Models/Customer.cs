717491e04a94a1dd6d2ceb176581acf0b3161670
Petru Betco <petrubetco@gmail.com>
2019-04-27 18:07:51 +0300

    Added validation properties

--- Vidly/Models/Customer.cs
+++ Vidly/Models/Customer.cs
@@ -9,14 +9,16 @@ namespace Vidly.Models
     public class Customer
     {
         public int Id { get; set; }
-        [Required]
+        [Required(ErrorMessage = "Please enter customer's name.")]
         [StringLength(255)]
         public string Name { get; set; }
         [Display(Name = "Date of Birth")]
+        [Min18YearsIfMember]
         public DateTime? Birthdate { get; set; }
         public bool IsSubscribedToNewsLetter { get; set; }
         public MembershipType MembershipType { get; set; }
         [Display (Name = "Membership Type")]
+        [Required(ErrorMessage = "Please specify a Membership type")]
         public byte MembershipTypeId { get; set; }  
     }
 }
